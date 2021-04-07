0aa603db8573d34bae68e9603a9fc17dbf60df63
Paul Lee <plee@ucsc.edu>
2021-04-07 14:26:41 -0700

    Starfield now scrolls!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -22,4 +22,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
     }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
+    }
 }
