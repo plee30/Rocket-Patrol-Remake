2a26bb7b14ebe9058909efd61e547059b8c12542
Paul Lee <plee@ucsc.edu>
2021-04-07 14:25:00 -0700

    Added assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,17 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/rocket.png');
+        this.load.image('starfield', 'assets/starfield.png');
+    }
+
     create() {
+
+        // starfield background
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
