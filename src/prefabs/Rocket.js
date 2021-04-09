b0ea10357976c0724189eb131c1477e01c6d2bf2
Paul Lee <plee@ucsc.edu>
2021-04-09 14:30:35 -0700

    Added basic collision

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,20 +1,43 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
+        super(scene, x, y, texture, frame);
 
-      // add object to existing scene
-      scene.add.existing(this);
-      
-      this.moveSpeed = 2;
+        // add object to existing scene
+        scene.add.existing(this);
+        
+        // Rocket movement speed
+        this.movementSpeed = 3;
+        
+        // Default isFiring to false
+        this.isFiring = false;
     }
 
     update() {
-        // TODO: only move if not firing
-        // TODO: clamp to visible area
-        if(keyLEFT.isDown) {
-            this.x -= this.moveSpeed;
-        } else if (keyRIGHT.isDown) {
-            this.x += this.moveSpeed;
+        if(this.isFiring) {
+            this.y -= this.movementSpeed;
+            if(this.y < borderUISize*3) {
+                this.reset();
+            }
+        } else {
+            // Left & Right Movement
+            if(keyLEFT.isDown) {
+                this.x -= this.movementSpeed;
+            } else if (keyRIGHT.isDown) {
+                this.x += this.movementSpeed;
+            }
+            
+            // If firing, set isFiring to true
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+            }
+
+            // Clamp to visible area
+            this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width-borderUISize-borderPadding);
         }
+    }
+
+    reset() {
+        this.y = game.config.height-borderUISize-borderPadding;
+        this.isFiring = false;
     }
   }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,6 +17,15 @@ class Play extends Phaser.Scene {
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 
+        // add ship1
+        this.ship1 = new Ship( this, 100, 200, 'spaceship' );
+
+        // add ship2
+        this.ship2 = new Ship( this, 300, 240, 'spaceship' );
+
+        // add ship3
+        this.ship3 = new Ship( this, 450, 300, 'spaceship' );
+
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
@@ -36,5 +45,20 @@ class Play extends Phaser.Scene {
     update() {
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
+
+        this.checkCollision(this.p1Rocket, this.ship1);
+        this.checkCollision(this.p1Rocket, this.ship2);
+        this.checkCollision(this.p1Rocket, this.ship3);
+    }
+
+    checkCollision(rocket, ship) {
+        if( rocket.x > ship.x && rocket.x < ship.x + ship.height && rocket.y > ship.y && rocket.y < ship.y + ship.height) {
+                ship.alpha = 0;
+                rocket.reset();
+                ship.reset();
+        }
     }
 }
