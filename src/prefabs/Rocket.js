feacdb40a7e126639bd7e4962498e1ff8884b934
Paul Lee <plee@ucsc.edu>
2021-04-09 18:29:49 -0700

    Finished the game

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,3 +1,4 @@
+// Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
@@ -5,39 +6,43 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // add object to existing scene
         scene.add.existing(this);
         
-        // Rocket movement speed
-        this.movementSpeed = 3;
-        
         // Default isFiring to false
         this.isFiring = false;
+
+        // Rocket movement speed
+        this.moveSpeed = 3;
+
+        // add rocket sfx
+        this.sfxRocket = scene.sound.add('sfx_rocket', {volume: 0.2});
     }
 
     update() {
-        if(this.isFiring) {
-            this.y -= this.movementSpeed;
-            if(this.y < borderUISize*3) {
-                this.reset();
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
             }
-        } else {
-            // Left & Right Movement
-            if(keyLEFT.isDown) {
-                this.x -= this.movementSpeed;
-            } else if (keyRIGHT.isDown) {
-                this.x += this.movementSpeed;
-            }
-            
-            // If firing, set isFiring to true
-            if(Phaser.Input.Keyboard.JustDown(keyF)) {
-                this.isFiring = true;
-            }
-
-            // Clamp to visible area
-            this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width-borderUISize-borderPadding);
         }
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play(); // play sfx
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+
     }
 
     reset() {
-        this.y = game.config.height-borderUISize-borderPadding;
         this.isFiring = false;
+        this.y = game.config.height-borderUISize-borderPadding;
     }
   }
